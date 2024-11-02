da519c1c6e04678ef76f914208f75d062c00f291
ABU THAGIR <86870385+abuthagir7185@users.noreply.github.com>
2024-11-02 20:43:04 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/script.js',  // Unga project main JS file
+    entry: 'C:\javascript programming\new projects\script.js',  // Unga project main JS file
     output: {
         filename: 'bundle.js',  // Output filename
         path: path.resolve(__dirname, 'dist'),
